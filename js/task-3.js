af97f09591eeab818cc10dffbc5e3707ccca7393
Roman <149504107+insdelhome@users.noreply.github.com>
2024-03-20 19:20:42 +0200

    Initial commit

---
+++ js/task-3.js
@@ -0,0 +1,22 @@
+const profile = {
+    username: "Jacob",
+    playTime: 300,
+    getInfo() {
+        // return `${profile.username} has ${profile.playTime} active hours!`
+        return `${this.username} has ${this.playTime} active hours!`
+    },
+    changeUsername(name) {
+        this.username = name;
+    },
+    updatePlayTime(time) {
+        this.playTime += time
+    }
+};
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
